b653a6d958f8792d5153267ef1db33594e0d3ec5
Alexander Kochemasov <akochemasov@gmail.com>
2018-10-13 09:22:53 +0400

    fix log

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const getCity = () => {
-  const city = readlineSync.question('Enter the name of the city ');
-  console.log(`Your city ${city}`);
+  const city = readlineSync.question('Enter the name of the city: ');
+  console.log(`Your city: ${city}`);
 };
 
 export default getCity;
