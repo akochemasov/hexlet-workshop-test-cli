0c52a786d78cfc7a7b313b637e9c139ff7f67a61
Alexander Kochemasov <akochemasov@gmail.com>
2018-10-13 09:15:38 +0400

    add prettier and fixes

--- src/bin/weather.js
+++ src/bin/weather.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
-console.log('Hello world');
+import getCity from '..';
+
+getCity();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Get city name');
+import readlineSync from 'readline-sync';
+
+const getCity = () => {
+  const city = readlineSync.question('Enter the name of the city ');
+  console.log(`Your city ${city}`);
+};
+
+export default getCity;
